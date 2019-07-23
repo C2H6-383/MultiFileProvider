61dbfed07741f7f04cb11c788a45178caa814d3e
C2H6-383 <ethan.finley@web.de>
2019-07-23 16:58:07 +0200

    Fixed Bugs, added more Path replacing features

--- MultiFileProvider.php
+++ MultiFileProvider.php
@@ -23,6 +23,12 @@ namespace {
 
         /**
          *
+         * @var string Replace Word for the Website root path 
+         */
+        private $rootWord;
+
+        /**
+         *
          * @var string the top adress for this Site (for autoreplace of CSS/JS include paths)
          */
         private $SiteTopAdress;
@@ -32,7 +38,8 @@ namespace {
          * @var string[] Contains all Allowed MIME Types (only these are tested with the class)
          */
         public static $allowedMimeTypes = [
-            "js" => "application/javascript", "css" => "text/css"
+            "js" => "application/javascript",
+            "css" => "text/css"
         ];
 
         /**
@@ -67,6 +74,14 @@ namespace {
 
             $this->SiteTopAdress = $SiteTopAdress;
             $this->mimeType = $mimeType;
+        }
+
+        /**
+         * sets the Root Word for Replace with the Website Path
+         * @param string $newWord the new word
+         */
+        public function set_RootWord($newWord) {
+            $this->rootWord = $newWord;
         }
 
         /**
@@ -158,7 +173,7 @@ namespace {
         private function outputNotModifiedHTTPHeaders($newestFile) {
             if (!$this->HeadersSent) {
                 $this->HeadersSent = true;
-                header("Content-Type: " . $this->mimeType);
+                header("Cache-Control: max-age=0, public, must-revalidate");
                 header('Last-Modified: ' . $newestFile->get_LastModified_date(true));
                 header('HTTP/1.0 304 Not Modified');
             }
@@ -172,6 +187,9 @@ namespace {
             echo "/*\n\n Dynamic File, Provided by MultiFileProvider $this->version \n File type: $this->mimeType \n File count: $FileCount \n\n #################################################\n\n*/\n\n";
         }
 
+        /**
+         * Outputs the visible Text footer for the file
+         */
         private function outputFileTextFooter() {
             echo "/*\n\n #################################################\n\n Dynamic File ended. \n MultiFileProvider -> (C) Ethan Ziermann, Germany \n https://github.com/C2H6-383/MultiFileProvider\n\n*/\n\n ";
         }
@@ -182,7 +200,8 @@ namespace {
          */
         private function outputFile($File) {
             $file_Content = file_get_contents($File->get_FileName());
-            $file_Content = str_replace("ROOT", $this->SiteTopAdress, $file_Content);
+            $file_Content = str_replace($this->rootWord, $this->SiteTopAdress, $file_Content);
+            $file_Content = str_replace("../", $this->SiteTopAdress, $file_Content);
             echo "\n";
             echo "/*\n#################################################\n";
             echo "File name: " . $File->get_FileName() . "  \n";
@@ -191,6 +210,11 @@ namespace {
             echo "" . $file_Content;
         }
 
+        /**
+         * Creates the Output for the full file
+         * @param MultiFileProvider\FileContainer $Files all Files for Output
+         * @param MultiFileProvider\FileContainer $newestFile the newest File of all Files
+         */
         private function createOutput($Files, $newestFile) {
             $this->outputStandardHTTPHeaders($newestFile);
             $this->outputFileTextHeader(count($Files));
@@ -204,6 +228,7 @@ namespace {
          * creates the File and sends it to the Browser
          */
         public function createMultiFile() {
+
             $RequestedKeywords = $this->getKeyWordsFromURL();
             foreach ($this->allFiles as $File) {
                 if (in_array($File->get_KeyWord(), $RequestedKeywords)) {
@@ -306,6 +331,11 @@ namespace MultiFileProvider {
             return $this->keyWord;
         }
 
+        /**
+         * gets the last Modification timestamp as date or GMT date
+         * @param bool $gmt should it be in GMT (default is false)
+         * @return string the date or gmdate
+         */
         public function get_LastModified_date($gmt = false) {
             if ($gmt) {
                 return gmdate('D, d M Y H:i:s', $this->LastModified) . ' GMT';
